6484c794d22111e23a624b8b2e0ff8a3eafd8f68
Matteo Monaci <matteomonaci1@gmail.com>
2024-03-10 21:59:29 +0100

    end day commit, fiexd navbar, section last announcements

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const $ = (s, o=document) => o.querySelector(s);
 const $$ = (s, o=document) => o.querySelectorAll(s)
 
-const ciao = document.querySelector('#ciao');
-const titleDue = document.querySelector('#titleDue');
-
 const lastAnnContainer = $('.last-ann-container')
-console.log(lastAnnContainer); 
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        const inViewport = entry.isIntersecting;
-        if (inViewport) {
-            console.log('ciao');
+        if (entry.isIntersecting){
+            entry.target.style.transitionDuration = "1.5s";
+            entry.target.style.transitionProperty = "all";
+            entry.target.style.margin = "0";
         }
-        entry.target.style.transitionDuration = "1s";
-        entry.target.style.transitionProperty = "all";
-        entry.target.style.width = inViewport ? "100%" : "80%";
     });
-}, { threshold: 0.35});
+}, { threshold: 0.25});
 
-observer.observe(ciao);
+// impostare threshold a 0.50 per schermi telefono
+
+observer.observe(lastAnnContainer);
