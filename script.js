525e865f60f81ee36aa01a6b1b9e4660b36b2c5d
Matteo Monaci <matteomonaci1@gmail.com>
2024-03-10 13:55:24 +0100

    half day commit, fixed bugs and start new section

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const $ = (s, o=document) => o.querySelector(s);
 const $$ = (s, o=document) => o.querySelectorAll(s)
+
+const ciao = document.querySelector('#ciao');
+const titleDue = document.querySelector('#titleDue');
+
+const lastAnnContainer = $('.last-ann-container')
+console.log(lastAnnContainer); 
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const inViewport = entry.isIntersecting;
+        if (inViewport) {
+            console.log('ciao');
+        }
+        entry.target.style.transitionDuration = "1s";
+        entry.target.style.transitionProperty = "all";
+        entry.target.style.width = inViewport ? "100%" : "80%";
+    });
+}, { threshold: 0.35});
+
+observer.observe(ciao);
